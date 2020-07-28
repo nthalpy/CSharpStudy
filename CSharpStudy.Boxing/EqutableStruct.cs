e4f313c09b9ec409fa7523102eb1bece9fca3e4e
Harnel <dcrgkev@gmail.com>
2020-07-28 20:31:14 +0900

    Add overiding of Object.Equals to EqutableStruct

--- CSharpStudy.Boxing/EqutableStruct.cs
+++ CSharpStudy.Boxing/EqutableStruct.cs
@@ -13,6 +13,20 @@ namespace CSharpStudy.Boxing
         private double a;
         private double b;
 
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(a, b);
+        }
+        public override bool Equals(object obj)
+        { 
+            if (obj is EqutableStruct)
+            {
+                EqutableStruct other = (EqutableStruct)obj;
+                return this.a == other.a && this.b == other.b;
+            }
+
+            return false;
+        }
         public bool Equals(EqutableStruct other)
         {
             return this.a == other.a && this.b == other.b;
